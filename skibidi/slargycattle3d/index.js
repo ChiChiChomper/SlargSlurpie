bfc49624a7bef6d3081bbaa5669067f5aa1170f2
Adlong Smithereens <aidenws715@gmail.com>
2025-04-20 22:15:19 -0400

    null

--- skibidi/slargycattle3d/index.js
+++ skibidi/slargycattle3d/index.js
@@ -312,7 +312,7 @@ const InternalConfig = function (initConfig) { // eslint-disable-line no-unused-
 		 * @default
 		 * @type {?string}
 		 */
-		mainPack: nil,
+		mainPack: null,
 		/**
 		 * Specify a language code to select the proper localization for the game.
 		 *
